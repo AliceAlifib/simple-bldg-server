34b21177fcfd9b6ea17b6e37e1a818c70e75c32c
Udi h Bauman <dibaunaumh@gmail.com>
2024-04-30 22:39:03 -0700

    protocol change: look now returns also the container bldg of the flr requested

--- bldg_server/lib/bldg_server_web/controllers/bldg_controller.ex
+++ bldg_server/lib/bldg_server_web/controllers/bldg_controller.ex
@@ -46,8 +46,10 @@ defmodule BldgServerWeb.BldgController do
   def look(conn, %{"flr" => flr}) do
     # unescape the flr parameter
     decoded_flr = URI.decode(flr)
+    container_bldg_addr = Buildings.get_flr_bldg(decoded_flr)
+    container = Buildings.get_bldg!(container_bldg_addr)
     bldgs = Buildings.list_bldgs_in_flr(decoded_flr)
-    render(conn, "look.json", bldgs: bldgs)
+    render(conn, "look.json", bldgs: [container | bldgs])
   end
 
 
